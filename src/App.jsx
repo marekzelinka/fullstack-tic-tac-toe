f0cdb4f43cefee9f3978b62c3f8af89699f89d4b
Marek Zelinka <mzelinka17@gmail.com>
2026-03-09 11:33:11 +0100

    refactor xIsNext logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
 
 export default function Game() {
-  const [xIsNext, setXISNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXISNext(!xIsNext);
   };
 
   const jumpTo = (nextMove) => {
     setCurrentMove(nextMove);
-    setXISNext(nextMove % 2 === 0);
   };
 
   const moves = history.map((squares, move) => {
